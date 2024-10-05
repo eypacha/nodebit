7adf819c644e1c7339237a0f73258990a00a6baf
Ey Pacha! <pachaguionbajo@gmail.com>
2024-10-04 22:52:34 -0300

    refactor: no more constantes

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const NODE_TYPES = {
-    empty: { inputs: 0, outputs: 0 },
-    exp: { inputs: 1, outputs: 1},
-    conmut: { inputs: 1, outputs: 1},
-    number: { inputs: 1, outputs: 1 },
-    out: { inputs: 1, outputs: 0 },
-    switch: { inputs: 4, outputs: 1 },
-  };
