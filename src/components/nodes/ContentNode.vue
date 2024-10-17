c0066aef76dcf5e35df4d2df77a7252a7db1ec33
Ey Pacha! <pachaguionbajo@gmail.com>
2024-10-17 14:06:36 -0300

    feat: add content node

---
+++ src/components/nodes/ContentNode.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="content noselect" style="color: cyan !important">{{ content }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: String
+});
+</script>
