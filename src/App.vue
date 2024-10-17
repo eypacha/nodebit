16dce968b07e7e523b501c92b9427746b4182162
Ey Pacha! <pachaguionbajo@gmail.com>
2024-10-17 14:08:20 -0300

    styles: better header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-    <header class="noselect">nodebit 0.0001</header>
+    <header class="noselect">
+      <nav>
+        <img src="/public/favicon-32x32.png" alt="Nodebit" />  
+        <h4>nodebit 0.001</h4>
+      </nav>
+    </header>
+
     <main>
       <graph-canvas/>
     </main>
@@ -22,6 +28,18 @@ export default {
 <style scoped>
 header {
   padding: .5rem;
-  border-bottom: solid 1px grey;
+  color: cyan;
+  border-bottom: solid 1px #c542c157;
+
+  nav {
+    display: flex;
+    gap: .5em;
+    align-items: flex-end;
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+  }
 }
 </style>
