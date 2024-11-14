fa2967687f64f5b35e5564ffe9d2ed0874467fac
Ey Pacha! <pachaguionbajo@gmail.com>
2024-11-13 22:27:24 -0300

    feat(audioEngine): add custom SampleRate

--- src/services/audioEngineService.js
+++ src/services/audioEngineService.js
@@ -2,42 +2,58 @@ class AudioEngine {
   constructor() {
     this.context = null;
     this.byteBeatNode = null;
+    this.sampleTime = 8000;
     this.isPlaying = false;
     this.position = 0;
     this.lastUpdateTime = 0;
   }
 
   /**
-     * Initializes the audio context and sets up the ByteBeatNode.
-     * Automatically called on first play().
-     * @throws {Error} If there's an issue initializing the audio context.
-     */
+   * Initializes the audio context and sets up the ByteBeatNode.
+   * Automatically called on first play().
+   * @throws {Error} If there's an issue initializing the audio context.
+   */
   async initialize() {
-
-    if(!this.byteBeatNode) {
+    if (!this.byteBeatNode) {
       this.context = new (window.AudioContext || window.webkitAudioContext)();
       await this.context.resume();
       await ByteBeatNode.setup(this.context);
       this.byteBeatNode = new ByteBeatNode(this.context);
       this.byteBeatNode.setType(ByteBeatNode.Type.byteBeat);
       this.byteBeatNode.setExpressionType(ByteBeatNode.ExpressionType.infix);
-      this.byteBeatNode.setDesiredSampleRate(8000);
+      this.byteBeatNode.setDesiredSampleRate(this.sampleTime);
 
       this.stack = await this.byteBeatNode.createStack();
-      return true
-    } 
+      return true;
+    }
   }
 
   /**
-     * Starts playing the ByteBeat.
-     * Initializes the audio context if not done already.
-     * @throws {Error} If an error occurs during playback.
-     */
+   * Sets the desired sample rate for the ByteBeat.
+   * @param {number} sampleTime - The desired sample rate in seconds.
+   * @throws {Error} If an error occurs during setting the sample rate.
+   * */
+  setSampleRate(sampleTime = 8000) {
+    try {
+      this.sampleTime = sampleTime;
+      if (this.byteBeatNode) {
+        this.byteBeatNode.setDesiredSampleRate(sampleTime);
+      }
+    } catch (error) {
+      console.error("Error setting sample rate:", error);
+      throw error;
+    }
+  }
+  /**
+   * Starts playing the ByteBeat.
+   * Initializes the audio context if not done already.
+   * @throws {Error} If an error occurs during playback.
+   */
   async play() {
     if (this.isPlaying) return false;
 
-    if(!this.byteBeatNode) {
-      await this.initialize()
+    if (!this.byteBeatNode) {
+      await this.initialize();
     }
 
     try {
@@ -47,7 +63,7 @@ class AudioEngine {
       this.isPlaying = true;
       return true; // Retorna true si se ha comenzado a reproducir
     } catch (error) {
-      console.error('Error starting ByteBeat:', error);
+      console.error("Error starting ByteBeat:", error);
       throw error;
     }
   }
@@ -57,8 +73,8 @@ class AudioEngine {
    * If audio is not playing, this method has no effect.
    */
   async pause() {
-    if(!this.byteBeatNode) {
-      await this.initialize()
+    if (!this.byteBeatNode) {
+      await this.initialize();
     }
     if (!this.isPlaying) return false;
     this.byteBeatNode.disconnect();
@@ -71,14 +87,14 @@ class AudioEngine {
    * If audio is not playing, just reset the timer
    */
   async stop() {
-    if(!this.byteBeatNode) return true
+    if (!this.byteBeatNode) return true;
     this.byteBeatNode.reset();
-    if (!this.isPlaying) return 
+    if (!this.isPlaying) return;
     this.byteBeatNode.disconnect();
     this.isPlaying = false;
-    return true
+    return true;
   }
-  
+
   async getSamplesForVisualization(width) {
     if (!this.byteBeatNode) {
       await this.initialize();
@@ -90,13 +106,31 @@ class AudioEngine {
 
     if (this.isPlaying) {
       const startTime = this.position;
-      const endTime = startTime + elapsedTimeMS * 0.001 * this.byteBeatNode.getDesiredSampleRate() | 0;
-      const duration = (endTime - startTime);
+      const endTime =
+        (startTime +
+          elapsedTimeMS * 0.001 * this.byteBeatNode.getDesiredSampleRate()) |
+        0;
+      const duration = endTime - startTime;
       this.position = endTime;
 
       const context = await this.byteBeatNode.createContext();
-      const leftValues = await this.byteBeatNode.getSamplesForTimeRange(startTime, endTime, width, context, this.stack, 0);
-      const rightValues = await this.byteBeatNode.getSamplesForTimeRange(startTime, endTime, width, context, this.stack, 1);
+
+      const leftValues = await this.byteBeatNode.getSamplesForTimeRange(
+        startTime,
+        endTime,
+        width,
+        context,
+        this.stack,
+        0
+      );
+      const rightValues = await this.byteBeatNode.getSamplesForTimeRange(
+        startTime,
+        endTime,
+        width,
+        context,
+        this.stack,
+        1
+      );
 
       return { left: leftValues, right: rightValues };
     }
@@ -106,7 +140,7 @@ class AudioEngine {
   /**
    * Reset the timer
    */
-  async reset(){
+  async reset() {
     this.position = 0;
     this.lastUpdateTime = 0;
 
@@ -115,17 +149,20 @@ class AudioEngine {
     }
   }
 
+  /**
+   *
+   * @returns the current time in seconds
+   */
   getTime() {
-
-    if(!this.byteBeatNode) return 0
+    if (!this.byteBeatNode) return 0;
 
     const time = this.byteBeatNode.getTime() ?? 0;
-    return time
+    return time;
   }
 
   /**
    * Sets new expressions for the ByteBeat audio.
-   * @param {string[]} expressions - An array of two strings representing 
+   * @param {string[]} expressions - An array of two strings representing
    *                                 the expressions for left and right channels.
    * @throws {Error} If the ByteBeatNode is not initialized.
    * @example
@@ -136,7 +173,6 @@ class AudioEngine {
       this.byteBeatNode.setExpressions(expressions);
     }
   }
-
 }
 
 export const audioEngine = new AudioEngine();
